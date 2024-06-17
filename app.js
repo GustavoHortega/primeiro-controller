ee6d58c8cca2b28d5b49122f5387a38a7faf4dd7
Gustavo Hortega <gustavohortega80@gmail.com>
2024-06-17 17:33:35 -0400

    Adicione rotas principal e pedidos

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const routerPincipal = require('./routes/principal');
+const routerPedidos = require('./routes/pedidos');
 
 const app = express();
 const porta = 3000;
+
+app.use('/', routerPincipal);
+app.use('/pedidos', routerPedidos);
+
 
 app.listen(porta, () =>{
     console.log(`servidor ouvindo na porta ${porta}`);
